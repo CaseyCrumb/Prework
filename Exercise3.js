470f35b590a59a50edb372b188415f27f1325461
CaseyCrumb <caseyncrumb@gmail.com>
2023-08-13 13:03:36 -0500

    Create Exercise3.js

---
+++ Exercise3.js
@@ -0,0 +1,7 @@
+// Exercise 3
+console.log("My name is: ");
+console.log('Bill'); //change this line
+console.log("My age is: ");
+console.log(55); //change this line, which type should you use?
+console.log("My zip code is: "); 
+console.log(10101); //change this line, which type should you use?
