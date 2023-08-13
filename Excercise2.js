486364f27fde0e39c380bf78a20cf5c9cd74ff06
CaseyCrumb <caseyncrumb@gmail.com>
2023-08-13 12:53:42 -0500

    Create Excercise2.js

---
+++ Excercise2.js
@@ -0,0 +1,7 @@
+// Exercise 2.
+// console.log('Hello World!');
+/* console.log("There once was a writer of code");
+console.log('Who wanted to learn JS and node');
+console.log('They said "once I can learn,"');
+console.log("'I'll increase what I earn.'");
+consoel.log("So now they're in full programmer mode!");*/
