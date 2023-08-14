d5eaa34ad413dfa656cebbf035940b1eef8ac307
CaseyCrumb <caseyncrumb@gmail.com>
2023-08-14 14:56:06 -0500

    Create Exercise6.js

---
+++ Exercise6.js
@@ -0,0 +1,28 @@
+//Exercise 6
+console.log("true or false: wrapping in double quotes is the same as wrapping in single quotes");
+console.log("Hello world!" == 'Hello world!');
+
+console.log("true or false: using single quotes inside a string is the same as using double quotes inside the string");
+console.log("He said 'no'!" == 'He said "no"!');
+
+console.log("true or false: strings that say the same thing but have different capitalization are not equal");
+console.log("Hello world!" != "hello world!");
+
+console.log("true or false: in javascript, a string is considered less than another string if it would come before it in alphabetical order");
+console.log("apple" < "banana");
+
+console.log("The number 5 is the same value as the string '5'");
+console.log(5 == "5");
+
+console.log("The number 5 is the same value and same data type as the string '5'");
+console.log(5 === "5");
+console.log('wraping in double quotes is the same as wrapping in single quotes');
+console.log("Wow!" == 'Wow!'); // ==, true
+console.log("Five is less than than four.");
+console.log("5" < '4'); // <, false
+console.log('The number 5 and the number 5 are the not the same vaule or the same type');
+console.log(5 !== 5); // !==, false
+console.log("The number 6 and the number 7 are not equal")
+console.log(6 != 7); // !=, true
+console.log("The string 'Bat' the same value and same data type as the string 'Bat'")
+console.log('Bat' === 'Bat'); // ===, true
