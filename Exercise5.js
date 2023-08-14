0c17b51fe4ab09fd20603eed0ffdac4dbb7c2b02
CaseyCrumb <caseyncrumb@gmail.com>
2023-08-14 13:27:31 -0500

    Create Exercise5.js

---
+++ Exercise5.js
@@ -0,0 +1,11 @@
+//Exercise 5
+console.log("True or false: 5 is less than 2");
+console.log(5 < 2);
+console.log("True or false: 19 is not equal 11");
+console.log(19 != 11)
+console.log("True or false: 10 is greater than or equal to 10");
+console.log(10 >= 10);
+console.log(6 == 6); // ==, true
+console.log(11 > 12); // >, false
+console.log(22 <= 10); // <=, false
+console.log(5 != 4); // !=, true
