7b8951c4dd359bd0dd201dba72a22b098bdc3156
CaseyCrumb <caseyncrumb@gmail.com>
2023-08-29 18:42:38 -0500

    ex3

--- Exercise3.js
+++ Exercise3.js
@@ -4,4 +4,4 @@ console.log('Bill'); //change this line
 console.log("My age is: ");
 console.log(55); //change this line, which type should you use?
 console.log("My zip code is: "); 
-console.log(10101); //change this line, which type should you use?
+console.log("10101"); //change this line, which type should you use?
